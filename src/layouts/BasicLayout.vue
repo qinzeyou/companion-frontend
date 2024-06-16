15f871b6a6ed734f49b4eb919ed7c860d309d55a
qzy <3010149465@qq.com>
2024-06-16 11:02:14 +0800

    1. 修改router配置错误

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,11 +1,15 @@
 <script lang="ts" setup>
-import {showToast} from "vant";
-import {ref} from 'vue'
+import {ref} from 'vue';
+import {useRouter} from "vue-router";
 
+const router = useRouter();
 const active = ref('home');
 
 const onClickLeft = () => history.back();
-const onClickRight = () => showToast('按钮');
+// 搜索按钮点击事件
+const onSearchBtn = () => {
+    router.push('/search');
+};
 
 </script>
 
@@ -14,17 +18,17 @@ const onClickRight = () => showToast('按钮');
         title="标题"
         left-arrow
         @click-left="onClickLeft"
-        @click-right="onClickRight"
+        @click-right="onSearchBtn"
     >
         <template #right>
             <van-icon name="search" size="18" />
         </template>
     </van-nav-bar>
     <router-view />
-    <van-tabbar v-model="active">
-        <van-tabbar-item name="home" icon="home-o">主页</van-tabbar-item>
-        <van-tabbar-item name="team" icon="search">组队</van-tabbar-item>
-        <van-tabbar-item name="user" icon="friends-o">个人</van-tabbar-item>
+    <van-tabbar route v-model="active">
+        <van-tabbar-item to="/home" name="home" icon="home-o">主页</van-tabbar-item>
+        <van-tabbar-item to="/team" name="team" icon="search">组队</van-tabbar-item>
+        <van-tabbar-item to="/user" name="user" icon="friends-o">个人</van-tabbar-item>
     </van-tabbar>
 </template>
 
